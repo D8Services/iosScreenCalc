1ff16e10f28097be8c37b5bdb46ecfa5ed2d8ea7
ttyler <ttyler@mac.com>
2021-04-07 10:35:04 +0700

    Initial Creation 7-APR-2021

---
+++ screenCalc.py
@@ -0,0 +1,313 @@
+#!/usr/bin/env python3
+	
+def screenCal(model):
+	if model == "iPad1,1":
+		y = 1024
+		x = 768
+	if model == "iPad1,1":
+		y = 768
+		x = 768
+	if model == "iPad2,1":
+		y = 1024
+		x = 768
+	if model == "iPad2,4":
+		y = 1024
+		x = 768
+	if model == "iPad2,2":
+		y = 1024
+		x = 768
+	if model == "iPad2,3":
+		y = 1024
+		x = 768
+	if model == "iPad3,1":
+		y = 2048
+		x = 1536
+	if model == "iPad3,3":
+		y = 2048
+		x = 1536
+	if model == "iPad3,2":
+		y = 2048
+		x = 1536
+	if model == "iPad3,4":
+		y = 2048
+		x = 1536
+	if model == "iPad3,5":
+		y = 2048
+		x = 1536
+	if model == "iPad3,6":
+		y = 2048
+		x = 1536
+	if model == "iPad2,5":
+		y = 1024
+		x = 768
+	if model == "iPad2,6":
+		y = 1024
+		x = 768
+	if model == "iPad2,7":
+		y = 1024
+		x = 768
+	if model == "iPad4,1":
+		y = 2048
+		x = 1536
+	if model == "iPad4,2":
+		y = 2048
+		x = 1536
+	if model == "iPad4,2":
+		y = 2048
+		x = 1536
+	if model == "Pad4,4":
+		y = 2048
+		x = 1536
+	if model == "iPad4,5":
+		y = 2048
+		x = 1536
+	if model == "iPad4,5":
+		y = 2048
+		x = 1536
+	if model == "iPad5,3":
+		y = 2048
+		x = 1536
+	if model == "iPad5,4":
+		y = 2048
+		x = 1536
+	if model == "iPad4,7":
+		y = 2048
+		x = 1536
+	if model == "iPad4,8":
+		y = 2048
+		x = 1536
+	if model == "iPad4,8":
+		y = 2048
+		x = 1536
+	if model == "iPad5,1":
+		y = 2048
+		x = 1536
+	if model == "iPad5,2":
+		y = 2048
+		x = 1536
+	if model == "iPad6,7":
+		y = 2732
+		x = 2048
+	if model == "iPad6,8":
+		y = 2732
+		x = 2048
+	if model == "iPad6,3":
+		y = 2048
+		x = 1536
+	if model == "iPad6,4":
+		y = 2048
+		x = 1536
+	if model == "iPad6,4":
+		y = 2048
+		x = 1536
+	if model == "iPad6,11":
+		y = 2048
+		x = 1536
+	if model == "iPad6,12":
+		y = 2048
+		x = 1536
+	if model == "iPad7,3":
+		y = 2224
+		x = 1668
+	if model == "iPad7,4":
+		y = 2224
+		x = 1668
+	if model == "iPad7,1":
+		y = 2732
+		x = 2048
+	if model == "iPad7,2":
+		y = 2732
+		x = 2048
+	if model == "iPad7,5":
+		y = 2048
+		x = 1536
+	if model == "iPad7,6":
+		y = 2048
+		x = 1536
+	if model == "iPad8,1":
+		y = 2388
+		x = 1668
+	if model == "iPad8,2":
+		y = 2388
+		x = 1668
+	if model == "iPad8,3":
+		y = 2388
+		x = 1668
+	if model == "iPad8,4":
+		y = 2388
+		x = 1668
+	if model == "iPad8,5":
+		y = 2732
+		x = 2048
+	if model == "iPad8,6":
+		y = 2732
+		x = 2048
+	if model == "iPad8,7":
+		y = 2732
+		x = 2048
+	if model == "iPad8,8":
+		y = 2732
+		x = 2048
+	if model == "iPad11,3":
+		y = 2224
+		x = 1668
+	if model == "iPad11,4":
+		y = 2224
+		x = 1668
+	if model == "iPad11,1":
+		y = 2048
+		x = 1536
+	if model == "iPad11,2":
+		y = 2048
+		x = 1536
+	if model == "iPad7,11":
+		y = 2160
+		x = 1620
+	if model == "iPad7,12":
+		y = 2160
+		x = 1620
+	if model == "iPad8,9":
+		y = 2388
+		x = 1668
+	if model == "iPad8,10":
+		y = 2388
+		x = 1668
+	if model == "iPad8,11":
+		y = 2732
+		x = 2048
+	if model == "iPad8,12":
+		y = 2732
+		x = 2048
+	if model == "iPad11,6":
+		y = 2160
+		x = 1620
+	if model == "iPad11,7":
+		y = 2160
+		x = 1620
+	if model == "iPad13,1":
+		y = 2360
+		x = 1640
+	if model == "iPad13,2":
+		y = 2360
+		x = 1640
+	if model == "iPhone1,1":
+		y = 320
+		x = 480
+	if model == "iPhone1,2":
+		y = 320
+		x = 480
+	if model == "iPhone2,1":
+		y = 320
+		x = 480
+	if model == "iPhone3,1":
+		y = 960
+		x = 640
+	if model == "iPhone3,2":
+		y = 960
+		x = 640
+	if model == "iPhone3,3":
+		y = 960
+		x = 640
+	if model == "iPhone4,1":
+		y = 960
+		x = 640
+	if model == "iPhone5,1":
+		y = 1136
+		x = 640
+	if model == "iPhone5,2":
+		y = 1136
+		x = 640
+	if model == "iPhone5,2":
+		y = 1136
+		x = 640
+	if model == "iPhone5,3":
+		y = 1136
+		x = 640
+	if model == "iPhone5,4":
+		y = 1136
+		x = 640
+	if model == "iPhone6,1":
+		y = 1136
+		x = 640
+	if model == "iPhone6,2":
+		y = 1136
+		x = 640
+	if model == "iPhone7,2":
+		y = 1334
+		x = 750
+	if model == "iPhone7,1":
+		y = 1920
+		x = 1080
+	if model == "iPhone8,1":
+		y = 1334
+		x = 750
+	if model == "iPhone8,2":
+		y = 1920
+		x = 1080
+	if model == "iPhone8,4":
+		y = 1136
+		x = 640
+	if model == "iPhone9,1":
+		y = 1334
+		x = 750
+	if model == "iPhone9,3":
+		y = 1334
+		x = 750
+	if model == "iPhone9,2":
+		y = 1920
+		x = 1080
+	if model == "iPhone9,4":
+		y = 1920
+		x = 1080
+	if model == "iPhone10,1":
+		y = 1334
+		x = 750
+	if model == "iPhone10,4":
+		y = 1334
+		x = 750
+	if model == "iPhone10,2":
+		y = 1920
+		x = 1080
+	if model == "iPhone10,5":
+		y = 1920
+		x = 1080
+	if model == "iPhone10,3":
+		y = 2436
+		x = 1125
+	if model == "iPhone10,6":
+		y = 2436
+		x = 1125
+	if model == "iPhone11,2":
+		y = 2436
+		x = 1125
+	if model == "iPhone11,6":
+		y = 2688
+		x = 1242
+	if model == "iPhone11,8":
+		y = 1792
+		x = 828
+	if model == "iPhone12,1":
+		y = 1792
+		x = 828
+	if model == "iPhone12,3":
+		y = 2436
+		x = 1125
+	if model == "iPhone12,5":
+		y = 2688
+		x = 1242
+	if model == "iPhone12,8":
+		y = 1334
+		x = 750
+	if model == "iPhone13,2":
+		y = 2532
+		x = 1170
+	if model == "iPhone13,3":
+		y = 2532
+		x = 1170
+	if model == "iPhone13,1":
+		y = 2340
+		x = 1080
+	if model == "iPhone13,4":
+		y = 2778
+		x = 1284
+	return x, y
